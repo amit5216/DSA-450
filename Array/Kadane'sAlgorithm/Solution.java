9c669750935456ee12560e98f8e22d6ee11103a6
amit5216 <it.1803248@gmail.com>
2026-04-10 21:10:15 +0530

    Kadane's Algorithm

--- Array/Kadane'sAlgorithm/Solution.java
+++ Array/Kadane'sAlgorithm/Solution.java
@@ -7,11 +7,9 @@ public class Solution {
         int currSum = arr[0];
 
         for(int i = 1; i < arr.length; i++) {
-
             currSum = Math.max(arr[i], currSum + arr[i]);
             maxSum = Math.max(maxSum, currSum);
         }
-
         return maxSum;
     }
 }
