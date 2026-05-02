5bd960065943447657757f35cc47549a8c48fc2c
amit5216 <it.1803248@gmail.com>
2026-05-02 23:46:17 +0530

    Solution updated

--- String/Subsequences of String/Solution.java
+++ String/Subsequences of String/Solution.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 class Solution {
     public static ArrayList<String> subsequences(String str) {
         // Write your code here
-        ArrayList<String> result = new ArrayList<>();
-        helper(str, 0, "", result);
-        return result;
+        ArrayList<String> res = new ArrayList<>();
+        helper(str, 0, "", res);
+        return res;
     }
     
-    private static void helper(String str, int index, String current, ArrayList<String> result) {
+    private static void helper(String str, int index, String current, ArrayList<String> res) {
         
         // Base case
         if (index == str.length()) {
             if (!current.equals("")) {   // skip empty subsequence
-                result.add(current);
+                res.add(current);
             }
             return;
         }
         
         // Include current character
-        helper(str, index + 1, current + str.charAt(index), result);
+        helper(str, index + 1, current + str.charAt(index), res);
         
         // Exclude current character
-        helper(str, index + 1, current, result);
+        helper(str, index + 1, current, res);
     }
 }
