5d07decd7b167a0e423036ef5811841a5484f2ad
amit5216 <it.1803248@gmail.com>
2026-05-07 18:00:00 +0530

    updated sol

--- String/Palindrome String/Solution.java
+++ String/Palindrome String/Solution.java
@@ -1,3 +1,27 @@
+/* public static int isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        // Continue looping while the two pointers
+        // have not crossed
+        while (left < right) {
+          
+            // If the characters at the current positions
+            // are not equal
+            if (s.charAt(left) != s.charAt(right))
+                return 0;
+
+            // Move the left pointer to the right and
+            // the right pointer to the left
+            left++;
+            right--;
+        }
+
+        // If no mismatch is found, return 1 (palindrome)
+        return 1;
+    }
+*/
+
 class Solution {
     boolean isPalindrome(String s) {
         // code here
